ce0b6e1dc55b255c931201f0dfff63f9f43aa779
Javi H. Gil <javihgil@softspring.es>
2024-01-16 15:29:16 +0100

    Fix article update form

--- src/Form/Admin/Article/ArticleUpdateForm.php
+++ src/Form/Admin/Article/ArticleUpdateForm.php
@@ -3,14 +3,14 @@
 namespace Softspring\CmsBlogPlugin\Form\Admin\Article;
 
 use Softspring\CmsBlogPlugin\Model\ArticleAuthorInterface;
-use Softspring\CmsBundle\Form\Admin\Content\ContentCreateForm;
+use Softspring\CmsBundle\Form\Admin\Content\ContentUpdateForm;
 use Softspring\CmsBundle\Form\Type\UserType;
 use Softspring\CmsBundle\Manager\ContentManagerInterface;
 use Softspring\CmsBundle\Translator\TranslatableContext;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class ArticleUpdateForm extends ContentCreateForm
+class ArticleUpdateForm extends ContentUpdateForm
 {
     public function __construct(protected ContentManagerInterface $contentManager, TranslatableContext $translatableContext)
     {
