19db574a77adf32a7a05db23e7df2256759e64ca
AritzPalaciosOrtiz <46780004+AritzPalaciosOrtiz@users.noreply.github.com>
2026-05-27 09:16:08 +0200

    ADD module related articles (#20)

---
+++ src/Form/Type/BlogArticleTagType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Softspring\CmsBlogPlugin\Form\Type;
+
+use Softspring\CmsBlogPlugin\Manager\ArticleTagManager;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BlogArticleTagType extends AbstractType
+{
+    public function __construct(protected ArticleTagManager $articleTagManager)
+    {
+    }
+
+    public function getParent(): string
+    {
+        return ChoiceType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $tags = $this->articleTagManager->getExistingTags();
+        $choices = array_combine($tags, $tags) ?: [];
+
+        $resolver->setDefaults([
+            'required' => false,
+            'placeholder' => 'block_articles_related.form.tag.placeholder',
+            'choices' => $choices,
+            'choice_translation_domain' => false,
+        ]);
+    }
+}
