eca5a3275958374c0d0ce7485cc74377eb32f3f4
Javi H. Gil <javihgil@softspring.es>
2026-06-18 11:58:30 +0200

    Fix code style

--- src/DependencyInjection/SfsCmsBlogExtension.php
+++ src/DependencyInjection/SfsCmsBlogExtension.php
@@ -11,9 +11,9 @@ use Softspring\CmsDataPlugin\SfsCmsDataPlugin;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class SfsCmsBlogExtension extends Extension implements PrependExtensionInterface
 {
